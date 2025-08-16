0263c331ee999e68311d9f0a18e3089e54ff9050
yangrenzhi123 <yangrenzhi123@163.com>
2025-08-16 17:55:58 +0800

    Update Test.java

--- core/src/main/java/com/yang/test/java/fastjson/Test.java
+++ core/src/main/java/com/yang/test/java/fastjson/Test.java
@@ -1,8 +1,19 @@
 package com.yang.test.java.fastjson;
 import com.alibaba.fastjson2.JSONObject;
+import com.alibaba.fastjson2.JSONPath;
+import com.alibaba.fastjson2.JSONReader;
+
 public class Test {
     public static void main(String[] args) {
         JSONObject o = new JSONObject();
-        System.out.println(o.toJSONString());
+        o.put("name", "yang");
+        o.put("age", 18);
+        String source = o.toJSONString();
+
+        JSONPath path = JSONPath.of("$.name");
+
+        JSONReader reader = JSONReader.of(source);
+
+        System.out.println(path.extract(reader));
     }
 }
